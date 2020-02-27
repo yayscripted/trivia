21ee63796d995d4cbeadd31c309b4ea993305b94
Student <student@Students-MacBook-Air.local>
2020-02-26 17:37:34 -0800

    handleAns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,8 @@ class App extends Component {
     super(props);
     this.state = {
       count : i,
-      color : "white"
+      color : "white",
+      cn: "answer"
     }
   }
 
@@ -22,21 +23,38 @@ class App extends Component {
       i++;
       this.setState({
         count : i,
+        color: "white",
+        cn : "answer"
       });
       console.log(i);
     //}
   }
 
-  colorChange(q) {
-    //if( q === )
-    this.setState({
-      color:"red"
-    })
+  handleAnswer = (index, correctIndex) => {
+    //const answer = event.target.value;
+    console.log(index);
+    if(index === correctIndex) {
+      this.setState({
+        color:"green"
+      })
+    }else{
+      this.setState({
+        color:"red"
+      })
+
+    }
+    
+    //}
   }
+
+    
+  
 
   
   render() {
     const q = this.props.app[this.state.count];
+    const ci = q.correct_choice_index;
+    console.log(q);
     return (
       <div className="app">
         Trivia!
@@ -45,15 +63,15 @@ class App extends Component {
         <h1> <Question ques={q.question_text}/> </h1>
         <div className="btnContainer">
           <div className="btnContainers">
-            <button className="ansButton"><Answer ans = {q.choices[0]}/></button>
-            <button className="ansButton"><Answer ans = {q.choices[1]}/></button>
+            <button className="ansButton" onClick={() => this.handleAnswer(0, ci)}><Answer ans = {q.choices[0]}/></button>
+            <button className="ansButton" onClick={() => this.handleAnswer(1, ci)}><Answer ans = {q.choices[1]}/></button>
           </div>
           <div className="btnContainers">
-            <button className="ansButton"><Answer ans = {q.choices[2]}/></button>
-            <button className="ansButton"><Answer ans = {q.choices[3]}/></button>
+            <button className="ansButton" onClick={() => this.handleAnswer(2, ci)}><Answer ans = {q.choices[2]}/></button>
+            <button className="ansButton" onClick={() => this.handleAnswer(3, ci)}><Answer ans = {q.choices[3]}/></button>
           </div>
         </div>
-        
+
         <button  className="switch" onClick={() => this.handleClick()}>Click</button>
       </div>
     );
