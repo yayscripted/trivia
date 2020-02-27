504f8e7607267432b4612f3e6aebad99e58f77e3
Student <student@Students-MacBook-Air.local>
2020-02-26 23:27:18 -0800

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,8 +12,6 @@ class App extends Component {
     super(props);
     this.state = {
       count : i,
-      color : "white",
-      cn: "answer"
     }
   }
 
@@ -23,18 +21,38 @@ class App extends Component {
       i++;
       this.setState({
         count : i,
-        color: "white",
-        cn : "answer"
+        button0: "white",
+        button1: "white",
+        button2: "white",
+        button3: "white"
       });
       console.log(i);
     //}
   }
 
-  colorChange(q) {
-    //if( q === )
+  handleAnswer(index, correctIndex) {
+    let obj = {};
+    let iButtonClass;
+    let cButtonClass;
+    
     this.setState({
-      color: "red"
+      button0 : "white",
+      button1 : "white",
+      button2 : "white",
+      button3 : "white"
     });
+
+    if(index === correctIndex) {
+      cButtonClass = "button" + index;
+      obj[cButtonClass] = "green"      
+      this.setState(obj);
+    }else{
+      iButtonClass = "button" + index;
+      cButtonClass = "button" + correctIndex;
+      obj[iButtonClass] = "red";
+      obj[cButtonClass] = "green";
+      this.setState(obj);
+    }
   }
 
     
@@ -43,8 +61,14 @@ class App extends Component {
   
   render() {
     const q = this.props.app[this.state.count];
-    const ci = q.correct_choice_index;
+    let ci = q.correct_choice_index;
     console.log(q);
+    let button0 = "ansButton " + this.state.button0;
+    let button1 = "ansButton " + this.state.button1;
+    let button2 = "ansButton " + this.state.button2;
+    let button3 = "ansButton " + this.state.button3;
+    
+
     return (
       <div className="app">
         <h1 className="title"> Trivia!</h1>
@@ -53,12 +77,12 @@ class App extends Component {
         <h1> <Question ques={q.question_text}/> </h1>
         <div className="btnContainer">
           <div className="btnContainers">
-            <button className="ansButton" onClick={() => this.handleAnswer(0, ci)}><Answer ans = {q.choices[0]}/></button>
-            <button className="ansButton" onClick={() => this.handleAnswer(1, ci)}><Answer ans = {q.choices[1]}/></button>
+            <button className={button0} onClick={() => this.handleAnswer(0, ci)}><Answer ans = {q.choices[0]}/></button>
+            <button className={button1} onClick={() => this.handleAnswer(1, ci)}><Answer ans = {q.choices[1]}/></button>
           </div>
           <div className="btnContainers">
-            <button className="ansButton" onClick={() => this.handleAnswer(2, ci)}><Answer ans = {q.choices[2]}/></button>
-            <button className="ansButton" onClick={() => this.handleAnswer(3, ci)}><Answer ans = {q.choices[3]}/></button>
+            <button className={button2} onClick={() => this.handleAnswer(2, ci)}><Answer ans = {q.choices[2]}/></button>
+            <button className={button3} onClick={() => this.handleAnswer(3, ci)}><Answer ans = {q.choices[3]}/></button>
           </div>
         </div>
 
